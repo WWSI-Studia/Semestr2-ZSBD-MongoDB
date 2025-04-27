bf7efe263e7d87964786fd1ef4ed9dc820f1a47a
KStrzechowski <strzechowski.konrad@gmail.com>
2025-04-27 21:03:34 +0200

    Update dump

--- db-to-db-migration/validation_rules.js
+++ db-to-db-migration/validation_rules.js
@@ -1,61 +1,6 @@
 var validationRules = {
   invoice: 'Brak reguł walidacji',
-  order: '{\n' +
-    '  "$jsonSchema": {\n' +
-    '    "bsonType": "object",\n' +
-    '    "required": [\n' +
-    '      "_id",\n' +
-    '      "address_id",\n' +
-    '      "department_id",\n' +
-    '      "invoice_number",\n' +
-    '      "order_date",\n' +
-    '      "order_status",\n' +
-    '      "products"\n' +
-    '    ],\n' +
-    '    "properties": {\n' +
-    '      "_id": {\n' +
-    '        "bsonType": "objectId"\n' +
-    '      },\n' +
-    '      "address_id": {\n' +
-    '        "bsonType": "objectId"\n' +
-    '      },\n' +
-    '      "department_id": {\n' +
-    '        "bsonType": "objectId"\n' +
-    '      },\n' +
-    '      "invoice_number": {\n' +
-    '        "bsonType": "string"\n' +
-    '      },\n' +
-    '      "order_date": {\n' +
-    '        "bsonType": "date"\n' +
-    '      },\n' +
-    '      "order_status": {\n' +
-    '        "bsonType": "string"\n' +
-    '      },\n' +
-    '      "products": {\n' +
-    '        "bsonType": "array",\n' +
-    '        "items": {\n' +
-    '          "bsonType": "object",\n' +
-    '          "properties": {\n' +
-    '            "product_id": {\n' +
-    '              "bsonType": "objectId"\n' +
-    '            },\n' +
-    '            "quantity": {\n' +
-    '              "bsonType": "int"\n' +
-    '            },\n' +
-    '            "status": {\n' +
-    '              "bsonType": "string"\n' +
-    '            }\n' +
-    '          },\n' +
-    '          "required": [\n' +
-    '            "product_id",\n' +
-    '            "quantity",\n' +
-    '            "status"\n' +
-    '          ]\n' +
-    '        }\n' +
-    '      }\n' +
-    '    }\n' +
-    '  }\n' +
-    '}',
+  order: 'Brak reguł walidacji',
   device: 'Brak reguł walidacji',
   department: 'Brak reguł walidacji',
   client: 'Brak reguł walidacji',
